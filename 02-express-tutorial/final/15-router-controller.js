cff3ec8a9519539e4c9d58252343499383ac89b6
Radhey <shahradhey90@gmail.com>
2022-01-28 11:41:37 -0600

    routes/controllers changes

--- 02-express-tutorial/final/15-router-controller.js
+++ 02-express-tutorial/final/15-router-controller.js
@@ -1,61 +1,61 @@
-let { people } = require('../data')
+let { people } = require("../data");
 
 const getPeople = (req, res) => {
-  res.status(200).json({ success: true, data: people })
-}
+  res.status(200).json({ success: true, data: people });
+};
 
 const createPerson = (req, res) => {
-  const { name } = req.body
+  const { name } = req.body;
   if (!name) {
     return res
       .status(400)
-      .json({ success: false, msg: 'please provide name value' })
+      .json({ success: false, msg: "please provide name value" });
   }
-  res.status(201).send({ success: true, person: name })
-}
+  res.status(201).send({ success: true, person: name });
+};
 
 const createPersonPostman = (req, res) => {
-  const { name } = req.body
+  const { name } = req.body;
   if (!name) {
     return res
       .status(400)
-      .json({ success: false, msg: 'please provide name value' })
+      .json({ success: false, msg: "please provide name value 2" });
   }
-  res.status(201).send({ success: true, data: [...people, name] })
-}
+  res.status(201).send({ success: true, data: [...people, name] });
+};
 
 const updatePerson = (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
+  const { id } = req.params;
+  const { name } = req.body;
 
-  const person = people.find((person) => person.id === Number(id))
+  const person = people.find((person) => person.id === Number(id));
 
   if (!person) {
     return res
       .status(404)
-      .json({ success: false, msg: `no person with id ${id}` })
+      .json({ success: false, msg: `no person with id ${id}` });
   }
   const newPeople = people.map((person) => {
     if (person.id === Number(id)) {
-      person.name = name
+      person.name = name;
     }
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
-}
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+};
 
 const deletePerson = (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
+  const person = people.find((person) => person.id === Number(req.params.id));
   if (!person) {
     return res
       .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` })
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
   }
   const newPeople = people.filter(
     (person) => person.id !== Number(req.params.id)
-  )
-  return res.status(200).json({ success: true, data: newPeople })
-}
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+};
 
 module.exports = {
   getPeople,
@@ -63,4 +63,4 @@ module.exports = {
   createPersonPostman,
   updatePerson,
   deletePerson,
-}
+};
